0d7167ac90ebd97a2d03b911f1d2608d92b8ce5b
wding <wwding23@gmail.com>
2011-11-17 22:07:04 +1100

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeReviewer::Application.config.secret_token = '84d596a01885dfa32732bcbcad02c64131fb01eff47822068c9aed3d12fedfa5a0bff21b5669311fd86ba54fdcfb971468fcd759510bd4adb760997793fd1b00'
